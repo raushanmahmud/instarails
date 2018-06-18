1ca3614127cb7e9c8a49735d6a9f1e3659ee8db0
Dagar <dagar@method.kz>
2018-06-18 14:02:35 +0600

    Add users controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
 	get "/contact", to: "pages#contact"
 	get "/about", to: "pages#about"
 	root "pages#index"
+	resources :users
+
 end
--- db/migrate/20180615093002_add_index_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddIndexToUsers < ActiveRecord::Migration[5.2]
-  def change
-  	add_index :users, :email
-  	add_index :users, :username
-  end
-end
