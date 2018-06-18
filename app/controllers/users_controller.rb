6e112faefa3f2485c5f47e226529d238cb9b6ff3
Dagar <dagar@method.kz>
2018-06-18 14:48:16 +0600

    Add avatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,16 @@ class UsersController < ApplicationController
 	end
 
 	def new
+		@user = User.new
 	end
 
 	def create
+		@user = User.new(user_params)
+		if @user.save
+			redirect_to @user
+		else
+			render :new
+		end
 	end
 
 	def edit
@@ -21,6 +28,12 @@ class UsersController < ApplicationController
 	end
 
 	def destroy
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:username, :email, :password, :avatar)
 	end
 
 end
